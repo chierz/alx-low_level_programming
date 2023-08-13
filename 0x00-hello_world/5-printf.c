5748b70bb4ecdb45bd20a100db7fc0859df8fd6f
Bonaventure <bonanwaeze@gmail.com>
2023-08-13 13:37:55 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+int main(void)
+	/**
+	  *main - entry point
+	  *return - (0) is success
+	  */
+{
+	printf("'with proper grammar, but the outcome is a piece of art,' \n")
+}
+	return (0)

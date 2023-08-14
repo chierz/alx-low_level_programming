5952f1e0956f78e5be3ed3a706af9b9a1688ee61
Bonaventure <bonanwaeze@gmail.com>
2023-08-14 05:44:06 -0700

    Message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-include <stdio.h>
+#include <stdio.h>
 int main(void)
 	/**
-	  *main - entry point
-	  *return - (0) is success
-	  */
+	 *main:  entry point of the program
+	 *return: (0) is success
+	 */
 {
-	printf("'with proper grammar, but the outcome is a piece of art,' \n")
+	printf("'with proper grammar, but the outcome is a piece of art,' \n");
+	return (0);
 }
-	return (0)

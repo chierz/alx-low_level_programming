bf8106fccc7156476623fdb544e2044f483f4533
Bonaventure <bonanwaeze@gmail.com>
2023-08-14 09:08:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
   *main - entry point
-  *puts - prints string to screen
-  *return - always 0 if (success) and a value greater if fail
+  *Return: always 0 if (success) and a value greater if fail
   */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	   puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

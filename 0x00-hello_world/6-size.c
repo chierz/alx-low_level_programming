13b8ce1d5d592efcfbc630d54426a029a99bf4d6
Bonaventure <bonanwaeze@gmail.com>
2023-08-14 18:04:18 -0700

    6-sizeofbytesherein

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-  *main - entry point
-  *Return: (0) success
-  */
-  int main(void)
+*main - entry point
+*Return: (0) success
+*/
+int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));

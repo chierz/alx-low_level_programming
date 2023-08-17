1febb817a22e5eb5890b73cf5d5acf0a59b5304c
Bonaventure <bonanwaeze@gmail.com>
2023-08-16 18:49:59 -0700

    Message

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main - print putchar,one letter at at time
+  *Return: 0 is susscess
+  */
+int main(void)
+{
+	putchar(95);
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}

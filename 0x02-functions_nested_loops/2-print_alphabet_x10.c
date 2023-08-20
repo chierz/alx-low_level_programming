a7993a4ef4cf771feb2e11cc9d29225bbee4ab89
Bonaventure <bonanwaeze@gmail.com>
2023-08-20 14:45:26 -0700

    Message

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints alphabet 10 times
+ *Return: 0 is success
+ */
+
+void print_alphabet_x10(void)
+{
+	int t;
+	char i;
+
+	for (t = 0 ; t <= 9 ; t++)
+	{
+		for (i = 'a' ; i <= 'z' ; i++)
+			_putchar (i);
+	}
+	_putchar ('\n');
+
+}

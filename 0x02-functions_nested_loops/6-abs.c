537863068563f013610b0d9abc61de38b6884a9c
Bonaventure <bonanwaeze@gmail.com>
2023-08-20 21:10:50 -0700

    Message

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_abs - computes absolute value iow makes all nums positive if they are not
+ *@n: the number
+ *Return: retvalue
+ */
+
+int _abs(int n)
+
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
+

400ef771eaf171de3211059d759a2aad9fd282e3
Bonaventure <bonanwaeze@gmail.com>
2023-08-21 15:31:21 -0700

    Message

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - resets the int to 98
+ *@*n: is the number that has ben reset
+ *Return: 0 is success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+readme at the root of this folder whic is ideally supposed to explain what each file does

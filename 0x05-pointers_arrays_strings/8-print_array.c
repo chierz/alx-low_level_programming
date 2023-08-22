dbeeb8516d14eb9babf8d5cdf5fbd38c9401bff4
Bonaventure <bonanwaeze@gmail.com>
2023-08-22 12:42:03 -0700

    Message

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-*
-*
-*
-*/
+ *print_array - prints n elements of an array of integers
+ *@a: array
+ *@n: number of times of the array
+ *Return: void
+ */
 
 void print_array(int *a, int n)
 
@@ -13,10 +15,9 @@ void print_array(int *a, int n)
 
 	for (i = 0 ; i < n ; i++)
 	{
-		printf(2)
+		printf("%d", a[i]);
+		if (i < n - 1)/*if i is less than '\0' */
+			printf(", ");
 	}
-	int arr[5] = {98, 402, -198, 298, -1024}
-	
-	n = arr
-		printf("%d", n);
+	printf("\n");
 }
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.com"
+
+/**
+ *
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+
+{
+	_strcpy{dest, src}
+}

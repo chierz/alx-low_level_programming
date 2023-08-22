784be45ee501987920768b60e86c3244a913f418
Bonaventure <bonanwaeze@gmail.com>
2023-08-21 21:59:01 -0700

    Message

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*
+*
+*
+*/
+
+void print_array(int *a, int n)
+
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf(2)
+	}
+	int arr[5] = {98, 402, -198, 298, -1024}
+	
+	n = arr
+		printf("%d", n);
+}

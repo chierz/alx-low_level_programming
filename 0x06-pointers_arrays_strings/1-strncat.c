2de32b54c9a00804b17e6f7ae2d903da79ae22b7
Bonaventure <bonanwaeze@gmail.com>
2023-08-24 16:22:21 -0700

    Message

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,8 @@ char *_strncat(char *dest, char *src, int n)
 	{
 	length_of_string++;
 	}
-	for (z = 0 ; z < n && src[z] != '\0' ; z++,length_of_string++ )
+	for (z = 0 ; z < n && src[z] != '\0' ; z++,
+			length_of_string++)
 		dest[length_of_string] = src[z];
 	dest[length_of_string] = '\0';
 	return (dest);

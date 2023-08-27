2f402c624605b6ee350263cadfbc086ece4cea38
Bonaventure <bonanwaeze@gmail.com>
2023-08-26 23:59:36 -0700

    Message

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,40 +1,37 @@
 #include "main.h"
-#include <stdbool.h> /* For using boolean data type*/
+#include <stdbool.h> /*Include the header for boolean data type*/
 
 /**
- * cap_string - Capitalizes the first letter of each word in a string
- * @str: Input string
+ * cap_string - Capitalizes all words in a string
+ * @str: The input string
+ *
  * Return: Pointer to the modified string
  */
 char *cap_string(char *str)
 {
-	int i = 0;
-	bool new_word = true;
+	bool new_word = true; /*Flag to track the start of a new word*/
 
-	while (str[i] != '\0')
+	for (int i = 0; str[i] != '\0'; i++)
 	{
-		/* Check if current character is a separator*/
+		if (new_word && (str[i] >= 'a' && str[i] <= 'z'))
+			/*Check if new word and lowercase letter*/
+		{
+			str[i] -= 32; /*Convert lowercase to uppercase*/
+			new_word = false; /* Reset the new_word flag*/
+		}
+
+		/*Check for separator characters: space, tab, newline, etc.*/
 		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
 				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
 				str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-				str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+				str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+				str[i] == '}')
 		{
-			new_word = true;
+			new_word = true; /* Set the new_word flag*/
 		}
-		/*Check if current character is an alphabet*/
-		else if ((str[i] >= 'a' && str[i] <= 'z') && new_word)
-		{
-			str[i] -= 32; /*Convert lowercase to uppercase*/
-			new_word = false;
-		}
-		else
-		{
-			new_word = false;
-		}
-
-		i++;
 	}
 
 	return (str);
 }
+
 

cddb33d4bc540b9d57bba36438cec0fbb9159ef4
chier <bonanwaeze@gmail.com>
2023-09-01 11:51:45 -0700

    message

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,34 +8,5 @@
  */
 int _atoi(char *s)
 {
-	int digit;
-	int result = 0, sign = 1, seen_digit = 0;
-
-	while (*s)
-	{
-		if ((*s == '-' || *s == '+') && (!seen_digit ||
-					(*(s - 1) != '-' && *(s - 1) != '+')))
-			sign = (*s++ == '-') ? -1 : 1;
-
-		if (*s >= '0' && *s <= '9')
-		{
-			seen_digit = 1;
-			digit = *s++ - '0';
-
-			if (result > (INT_MAX - digit) / 10)
-				return ((sign == 1) ? INT_MAX : INT_MIN);
-
-			result = result * 10 + digit;
-		}
-		else if (seen_digit)
-		{
-			break;
-		}
-		else
-		{
-			s++;
-		}
-	}
-
-	return (result * sign);
+	return ();
 }

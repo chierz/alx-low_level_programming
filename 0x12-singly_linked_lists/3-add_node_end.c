3516074c94c165a65376d89fccf561b8943b8417
chier <bonanwaeze@gmail.com>
2023-09-22 20:57:26 -0700

    Message

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: A pointer to a pointer to the head of the list.
+ * @str: The string to be added to the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *current;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = NULL; /* As this is the last node, its next should be NULL*/
+
+	if (*head == NULL)
+	{
+		*head = new_node; /* If the list is empty, set the new node as the head*/
+		return (new_node);
+	}
+
+	current = *head;
+
+	while (current->next != NULL)
+		current = current->next;
+
+	current->next = new_node;
+
+	return (new_node);
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - free allocsted space
+ * @head: pointer to head node
+ * Return: Number of nodes
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+	list_t *current = head;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp->str);
+		free(temp);
+	}
+}

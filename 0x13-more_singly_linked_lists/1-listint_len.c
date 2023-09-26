af7c564a17f2f5aeea39d584aed9c9eaee3fd013
chier <bonanwaeze@gmail.com>
2023-09-26 14:38:09 -0700

    element_num

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -11,6 +11,7 @@ size_t listint_len(const listint_t *h)
 	size_t  i = 0;
 
 	const listint_t *new = h;
+
 	if (h == NULL)
 		return (0);
 
@@ -19,6 +20,5 @@ size_t listint_len(const listint_t *h)
 		new = new->next;
 		i++;
 	}
-	/*printf("-> %d elements\n", i);*/
 	return (i);
 }

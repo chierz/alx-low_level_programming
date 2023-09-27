6da9e49913139e36c62e0d7e965de0a814919b6d
chier <bonanwaeze@gmail.com>
2023-09-26 17:07:14 -0700

    Message

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum all nodes
+ * @head: pointer to head node
+ * Return: N
+ */
+int sum_listint(listint_t *head)
+{
+	unsigned int n = 0;
+
+	if (!(head))
+		return (0);
+
+	while (head)
+	{
+		n = n + head->n;
+		head = head->next;
+	}
+
+	return (n);
+}

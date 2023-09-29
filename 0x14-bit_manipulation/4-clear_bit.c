03f9c00c3a720a69fe922b4b4ca13a9b22df8e13
chier <bonanwaeze@gmail.com>
2023-09-29 15:50:48 -0700

    returns the value of a bit at a given index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,26 +1,20 @@
 #include "main.h"
 
 /**
- * clear_bit - Sets the value of a bit to 0 at a given index.
- * @n: Pointer to the number.
- * @index: The index, starting from 0 of the bit to clear.
- *
- * Return: 1 if it worked, or -1 if an error occurred.
+ * clear_bit - clear  the value of a bit
+ * @n: A pointer to the bit.
+ * @index: The index to set the value at - indices start at 0.
+ * Return: 1 else -1
  */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask ;
-
-	/* Check if index is within valid range */
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		printf("Error: index out of range\n");
-		return -1; /* Error: Index out of range */
+		return (-1);
 	}
-
-	mask = 1UL << index; /* Shift 1 by index positions */
-	*n = *n & ~mask; /* Clear the bit at the specified index */
-
-	return 1;
+	*n &= ~(1UL << index);
+	return (1);
 }
 
